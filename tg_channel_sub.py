f6534d8faf66b9276edd5d0403850b33d57920dd
Tink <Tink@Ta.nk>
2023-08-08 00:12:35 +0800

    fix py

--- tg_channel_sub.py
+++ tg_channel_sub.py
@@ -15,6 +15,17 @@ class TgChannelSub(Plugin):
 
     def __init__(self, config: dict):
         super().__init__(config)
+        # 获取rss地址
+        self.rss_url = self.config.get("rssurl")  # 将 rss_url 作为实例变量
+        # 存储rss已有项目的链接
+        self.processed_links = set()
+        # 获取初始的 RSS 订阅内容，跳过处理
+        feed = feedparser.parse(self.rss_url)
+        #print(feed)
+        for entry in feed.entries:
+            self.processed_links.add(entry.link)
+            print(entry.link)
+        
         scheduler_thread = threading.Thread(target=self.start_schedule)
         scheduler_thread.start()
 
@@ -35,9 +46,9 @@ class TgChannelSub(Plugin):
 
     def start_schedule(self):
         schedule.every().minute.do(self.auto_send)
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+        #while True:
+        #    schedule.run_pending()
+        #    time.sleep(600)
 
     def auto_send(self):
         logger.info("Start TG Channel Sub Auto Send")
@@ -51,31 +62,13 @@ class TgChannelSub(Plugin):
             send_txt(content, group_chat)
             time.sleep(1)
 
-#    def reply(self) -> Reply:
-#        reply = Reply(ReplyType.TEXT, "Failed to get daily news")
-#        try:
-#            reply_content = self.daily_news()
-#            reply = Reply(ReplyType.TEXT, reply_content)
-#        except Exception as e:
-#            logger.error("Failed to get daily news")
-#        return reply
-
     def tg_channel_msg(self) -> str:
-        # 获取rss地址
-        rss_url = self.config.get("rssurl")
-        print(rss_url)
-        # 保存已获取消息的链接
-        processed_links = set()
-
-        # 获取初始的 RSS 订阅内容，跳过处理
-        feed = feedparser.parse(rss_url)
-        for entry in feed.entries:
-            processed_links.add(entry.link)
-            print(entry.link)
+        
+        formatted_msg = ""
         
         try:
             # 解析 RSS 订阅
-            feed = feedparser.parse(rss_url)
+            feed = feedparser.parse(self.rss_url)
     
             # 遍历订阅的条目，获取最新的消息
             for entry in feed.entries:
@@ -83,19 +76,21 @@ class TgChannelSub(Plugin):
                 entry_link = entry.link
     
                 # 如果链接不在已获取消息的集合中，处理消息并添加到集合中
-                if entry_link not in processed_links:
-                    processed_links.add(entry_link)
+                if entry_link not in self.processed_links:
+                    self.processed_links.add(entry.link)  # 使用成员变量
+                    print(f"{entry.link} added to the processed list")
     
                     # 获取消息标题和描述
                     entry_title = entry.title
                     entry_description = BeautifulSoup(entry.description, 'html.parser').get_text()
     
                     # 打印消息标题和描述
-                    print("标题:", entry_title)
-                    print("描述:", entry_description)
-                    print("=" * 40)
-                    tg_channel_msg += f"{entry_title} : {entry_description} \n"
+                    #print("标题:", entry_title)
+                    #print("描述:", entry_description)
+                    #print("=" * 40)
+                    formatted_msg += f"{entry_title} : {entry_description} \n\n\n"
+                    print(formatted_msg)
 
         except Exception as e:
             logger.error(f"Error occurred while fetching tg channel msg: {str(e)}")
-        return tg_channel_msg
+        return formatted_msg
